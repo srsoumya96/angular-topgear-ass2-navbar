0a7b22b06677c2c4ad1df2ee9215750a5ba950cd
srsoumya96 <sr.soumya96@gmail.com>
2020-01-29 19:06:54 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-214zzb
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-214zzb)
---
+++ src/app/nav-bar/nav-bar.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-bar',
+  templateUrl: './nav-bar.component.html',
+  styleUrls: ['./nav-bar.component.css']
+})
+export class NavBarComponent implements OnInit {
+  
+  @Output() tabNav = new EventEmitter<String>();
+  constructor() {
+   }
+
+   onClicking(tabName : string) {
+     this.tabNav.emit(tabName);
+   }
+
+  ngOnInit() {
+  }
+
+}
